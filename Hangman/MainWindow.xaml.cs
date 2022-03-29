76caaabefe10e1251ee34f2338d192388be8b83a
Roman Princ <u19.421@pslib.cloud>
2022-03-30 00:14:02 +0200

    Přidejte soubory projektu.

---
+++ Hangman/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hangman
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            int lineX1 = 10;
+            int lineX2 = 20;
+            for (int i = 0; i < 10; i++)
+            {
+                var line = new Line();
+
+                line.Stroke = new SolidColorBrush(Colors.Black);
+                line.StrokeThickness = 2;
+                line.X1 = lineX1;
+                line.X2 = lineX2;
+
+
+
+                textLines.Children.Add(line);
+
+                lineX1 += 30;
+                lineX2 += 40;
+            }
+            Console.WriteLine("bu");
+        }
+    
+    }
+}
---
+++ Hangman/ViewModels/MainVM.cs
@@ -0,0 +1,75 @@
+﻿using HangMan.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.ViewModels
+{
+    internal class MainVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public ParametrizedRelayCommand<string> Command { get; set; }
+        public List<string> Words = new List<string>();
+
+        public ObservableCollection<string> CorrectChars = new ObservableCollection<string>();
+
+        public List<string> GuessedChars = new List<string>();
+
+        public MainVM()
+        {
+            Words.Add("HOUBA");
+            Words.Add("STROM");
+            Words.Add("SLUNCE");
+            Words.Add("OKO");
+
+            Words.Add("MASO");
+            Words.Add("OPICE");
+
+            Words.Add("HANGMAN");
+
+
+            int rnd = _rnd.Next(0, Words.Count);
+            SelectedWord = Words[rnd];
+            char[] selectedwords = new char[SelectedWord.Length];
+            for (int i = 0; i < SelectedWord.Length; i++)
+            {
+                selectedwords[i] = SelectedWord[i];
+            }
+
+
+            Command = new ParametrizedRelayCommand<string>(
+            (znak) =>
+            {
+                GuessedChars.Add(znak);
+                Command.RaiseCanExecureChanged();
+                if (SelectedWord.Contains(znak))
+                {
+                    CorrectChars.Append(znak);
+                }
+
+            },
+            (znak) =>
+            {
+                return !GuessedChars.Contains(znak);
+            });
+
+            for (int i = 0; i < SelectedWord.Length; i++)
+            {
+
+            }
+        }
+        private Random _rnd = new Random();
+        public string SelectedWord { get; set;}
+            
+        
+    }
+}
